69b2134264bbbe69df8088eece9f3274aa93add6
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-10-30 00:27:15 +0900

    fix: setCookie on WebView when finished loading

--- shared/src/commonMain/kotlin/ui/component/MoimeWebView.kt
+++ shared/src/commonMain/kotlin/ui/component/MoimeWebView.kt
@@ -4,15 +4,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.annotation.InternalVoyagerApi
 import cafe.adriel.voyager.navigator.internal.BackHandler
 import com.multiplatform.webview.cookie.Cookie
 import com.multiplatform.webview.jsbridge.IJsMessageHandler
 import com.multiplatform.webview.jsbridge.rememberWebViewJsBridge
+import com.multiplatform.webview.web.LoadingState
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewNavigator
 import com.multiplatform.webview.web.rememberWebViewState
+import kotlinx.coroutines.flow.filter
 import ui.theme.Gray700
 
 @OptIn(InternalVoyagerApi::class)
@@ -28,22 +31,29 @@ fun MoimeWebView(
     val webViewState = rememberWebViewState(url)
     val jsBridge = rememberWebViewJsBridge(webViewNavigator)
 
-    LaunchedEffect(webViewState) {
-        accessToken?.let {
-            webViewState.cookieManager.setCookie(
-                url = url,
-                cookie = Cookie(
-                    name = "Authorization",
-                    value = "Bearer $it"
-                )
-            )
-        }
+    LaunchedEffect(Unit) {
         webViewState.webSettings.iOSWebSettings.apply {
             backgroundColor = Gray700
             bounces = false
             showVerticalScrollIndicator = false
             showHorizontalScrollIndicator = false
         }
+    }
+
+    LaunchedEffect(webViewState) {
+        snapshotFlow { webViewState.loadingState }
+            .filter { it is LoadingState.Finished }
+            .collect {
+                accessToken?.let {
+                    webViewState.cookieManager.setCookie(
+                        url = url,
+                        cookie = Cookie(
+                            name = "Authorization",
+                            value = "Bearer $it"
+                        )
+                    )
+                }
+            }
     }
 
     LaunchedEffect(jsBridge) {
