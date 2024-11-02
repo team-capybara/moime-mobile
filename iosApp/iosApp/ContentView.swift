4869dd855f060f9774c03ac3075a82d933cb19fd
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-11-02 15:24:58 +0900

    fix(ios): keyboard not shown on webview

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -14,6 +14,5 @@ struct ContentView: View {
     var body: some View {
         ComposeView()
                 .ignoresSafeArea(edges: .all)
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
