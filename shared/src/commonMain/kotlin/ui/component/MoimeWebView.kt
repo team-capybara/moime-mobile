8978bf6027c0c6f993b848fdb07f875c5bec5293
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-11-17 17:45:32 +0900

    fix: set cookie before webView loading

--- shared/src/commonMain/kotlin/ui/component/MoimeWebView.kt
+++ shared/src/commonMain/kotlin/ui/component/MoimeWebView.kt
@@ -4,18 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.annotation.InternalVoyagerApi
 import cafe.adriel.voyager.navigator.internal.BackHandler
 import com.multiplatform.webview.cookie.Cookie
 import com.multiplatform.webview.jsbridge.IJsMessageHandler
 import com.multiplatform.webview.jsbridge.rememberWebViewJsBridge
-import com.multiplatform.webview.web.LoadingState
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewNavigator
 import com.multiplatform.webview.web.rememberWebViewState
-import kotlinx.coroutines.flow.filter
 import ui.theme.Gray700
 
 @OptIn(InternalVoyagerApi::class)
@@ -31,20 +28,20 @@ fun MoimeWebView(
     val webViewState = rememberWebViewState(url)
     val jsBridge = rememberWebViewJsBridge(webViewNavigator)
 
-    LaunchedEffect(webViewState) {
-        snapshotFlow { webViewState.loadingState }
-            .filter { it is LoadingState.Finished }
-            .collect {
-                accessToken?.let {
-                    webViewState.cookieManager.setCookie(
-                        url = url,
-                        cookie = Cookie(
-                            name = "Authorization",
-                            value = "Bearer $it"
-                        )
+    LaunchedEffect(accessToken) {
+        with(webViewState.cookieManager) {
+            accessToken?.let { token ->
+                setCookie(
+                    url = url,
+                    cookie = Cookie(
+                        name = "Authorization",
+                        value = "Bearer $token"
                     )
-                }
+                )
+            } ?: run {
+                removeCookies(url)
             }
+        }
     }
 
     LaunchedEffect(jsBridge) {
@@ -54,18 +51,14 @@ fun MoimeWebView(
     }
 
     onDispose?.let {
-        BackHandler(webViewNavigator.canGoBack.not()) {
-            it()
-        }
+        BackHandler(webViewNavigator.canGoBack.not(), it)
     }
 
     SafeAreaColumn {
         Box {
             WebView(
                 state = webViewState.apply {
-                    webSettings.apply {
-                        backgroundColor = Gray700
-                    }
+                    webSettings.backgroundColor = Gray700
                 },
                 navigator = webViewNavigator,
                 webViewJsBridge = jsBridge,
