0d02e0119992d09f4d806ea96770950ed427bea8
yeojun-yoon <yjyoon.dev@navercorp.com>
2024-11-28 17:24:49 +0900

    chore: disable ExceptionDialog on release

--- shared/src/commonMain/kotlin/ui/component/ExceptionDialog.kt
+++ shared/src/commonMain/kotlin/ui/component/ExceptionDialog.kt
@@ -2,25 +2,22 @@ package ui.component
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import moime.shared.generated.resources.Res
-import moime.shared.generated.resources.api_exception
-import moime.shared.generated.resources.confirm
-import org.jetbrains.compose.resources.stringResource
 
+/** Only for debugging. */
 @Composable
 fun ExceptionDialog(
     exception: Throwable,
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    MoimeDialog(
-        request = DialogRequest(
-            title = stringResource(Res.string.api_exception),
-            description = exception.message ?: exception.stackTraceToString(),
-            actionTextRes = Res.string.confirm,
-            onAction = onDismiss
-        ),
-        onDismiss = onDismiss,
-        modifier = modifier
-    )
+//    MoimeDialog(
+//        request = DialogRequest(
+//            title = stringResource(Res.string.api_exception),
+//            description = exception.message ?: exception.stackTraceToString(),
+//            actionTextRes = Res.string.confirm,
+//            onAction = onDismiss
+//        ),
+//        onDismiss = onDismiss,
+//        modifier = modifier
+//    )
 }
